307eb8b1c12a7a53123e12c481db54309507c759
bilaluali-travelperk <bilal.uali@travelperk.com>
2022-01-31 18:02:15 +0100

    Defined tests for retrieving recipes that startswith

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -38,6 +38,29 @@ class RecipeApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
+    def test_list_recipes_startswith_name(self):
+        """Test retrieving a list of recipes starting with param
+        substring name"""
+        substr = 'Baked'
+        sample_recipe(name=f'{substr} Spaghetti')
+        sample_recipe(name=f'{substr} Mushroom')
+        sample_recipe(name=f'{substr} Chciken')
+        sample_recipe(name=f'Does not start with {substr}')
+
+        url = recipe_url('list')
+        res = self.client.get(url, {
+            'ordering': 'id',
+            'name': substr
+        })
+
+        recipes = Recipe.objects \
+            .filter(name__startswith=substr) \
+            .order_by('id')
+        serializer = RecipeSerializer(recipes, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
     def test_detail_recipe(self):
         """Test retrieving a detailed recipe"""
         recipe = sample_recipe()
